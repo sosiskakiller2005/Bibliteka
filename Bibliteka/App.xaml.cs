aa3846c49060951ee786702bf89615d7ebefc9c7
Влад Козлов <vkozlov15012005@gmail.com>
2024-11-08 12:08:48 +0300

    Добавьте файлы проекта.

---
+++ Bibliteka/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Bibliteka.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Bibliteka
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static BiblitekaEntities _context;
+        public static BiblitekaEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new BiblitekaEntities();
+            }
+            return _context;
+        }
+    }
+}
